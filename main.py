4ee934ab465e68c2291ccad2cb3a9a1593564016
Selvet Elif Demirel <selvet.elif@gmail.com>
2025-08-20 02:42:35 +0300

    Created main.py  file

---
+++ main.py
@@ -0,0 +1,42 @@
+from library import Library
+
+def main():
+    library = Library(name= "City Library")
+
+    while True:
+        print("\n--- Kitap Yönetim Sistemi ---")
+        print("1. Kitap Ekle")
+        print("2. Kitap Sil")
+        print("3. Kitapları Listele")
+        print("4. Kitap Ara")
+        print("5. Çıkış")
+
+        choice = input("Seçiminizi yapın: ")
+
+        if choice == "1":
+            title = input("Kitap adı: ")
+            author = input("Yazar: ")
+            isbn = input("ISBN: ")
+            library.add_book(title, author, isbn)
+        elif choice == "2":
+            isbn = input("Silmek istediğiniz kitabın ISBN'i: ")
+            library.remove_book(isbn)
+        elif choice == "3":
+            print("Kitap listesi:")
+            for info in library.list_books():
+                print("-", info)
+        elif choice == "4":
+            title = input("Aranacak kitabın adı: ")
+            result = library.find_book(title=title)
+            if result:
+                print(result)
+            else:
+                print("Kitap bulunamadı.")
+        elif choice == "5":
+            print("Programdan çıkılıyor...")
+            break
+        else:
+            print("Geçersiz seçim, tekrar deneyin.")
+
+if __name__ == "__main__":
+    main()
